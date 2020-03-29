cd3af96090bd39c95b9271ef64a76b18dee09a88
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-03-30 00:54:35 +0200

    Added custom objects subfolders to the editor

--- EditorAdditions/Harmony/global/AudioManager/MusicCue/ctor.cs
+++ EditorAdditions/Harmony/global/AudioManager/MusicCue/ctor.cs
@@ -1,9 +1,10 @@
 ﻿#pragma warning disable IDE0051, RCS1213
 using Harmony;
+using System;
 
 namespace EditorAdditions.Harmony
 {
-    [HarmonyPatch(typeof(AudioManager.MusicCue), MethodType.Constructor)]
+    [HarmonyPatch(typeof(AudioManager.MusicCue), MethodType.Constructor, new Type[] { typeof(MusicCueID), typeof(uint), typeof(string), typeof(bool) })]
     public static class ConstructMusicCue
     {
         private static void Postfix(AudioManager.MusicCue __instance)
--- EditorAdditions/Harmony/global/LibraryTab/Start.cs
+++ EditorAdditions/Harmony/global/LibraryTab/Start.cs
@@ -13,16 +13,18 @@ namespace EditorAdditions.Harmony
             __instance.iconSizeSlider_.onChange.Add(new EventDelegate(() => {
                 Plugin.Configuration["EditorIconSize"] = __instance.IconSize_;
                 Plugin.Configuration.Save();
-
-                Plugin.Log.Info("iconSizeSlider_.onChange");
             }));
 
             if (Plugin.Configuration.ContainsKey("EditorIconSize"))
+            {
                 __instance.iconSize_ = Plugin.Configuration.GetItem<float>("EditorIconSize");
+            }
 
             __instance.rootFileData_ = G.Sys.ResourceManager_.LevelPrefabFileInfosRoot_;
             if (Plugin.Configuration["DevFolderEnabled"] is false)
+            {
                 __instance.rootFileData_.RemoveAllChildInfos((LevelPrefabFileInfo x) => x.IsDirectory_ && x.Name_ == "Dev");
+            }
             __instance.currentDirectory_ = __instance.rootFileData_;
             __instance.iconSizeSlider_.value = Mathf.InverseLerp(32f, 256f, __instance.iconSize_);
             __instance.searchInput_ = __instance.GetComponentInChildren<UIExInput>();
