fed21426dbc91361f237a75497b1eef2b3422834
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-04-03 19:18:48 +0200

    Added "inspect parent" button to transform components

---
+++ EditorAdditions/Harmony/global/TransformWrapper/Visit.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace EditorAdditions.Harmony
+{
+    [HarmonyPatch(typeof(TransformWrapper), "Visit")]
+    internal static class VititTransform
+    {
+        internal static void Prefix(TransformWrapper __instance, IVisitor visitor)
+        {
+            if (!(visitor is Serializers.Serializer) && !(visitor is Serializers.Deserializer))
+            {
+                Transform ObjectTransform = __instance.tComponent_;
+                if (!ObjectTransform.IsRoot())
+                {
+                    visitor.VisitAction("Inspect Parent", () => {
+                        var Editor = G.Sys.LevelEditor_;
+                        var Selection = Editor.SelectedObjects_;
+                        if (Selection.Count == 1)
+                        {
+                            EditorToolset.Inspect(ObjectTransform.parent.gameObject);
+                        }
+                        else
+                        {
+                            G.Sys.MenuPanelManager_.ShowError("ERROR", "You must select only 1 object to ust this tool.");
+                        }
+                    }, null);
+                }
+            }
+        }
+    }
+}
